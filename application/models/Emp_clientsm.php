5ab6d5a06f8422b8a587f5923d4dfea7f41c1b2d
On1ty <juanity.andoy@gmail.com>
2021-01-04 13:07:56 +0800

    change query

--- application/models/Emp_clientsm.php
+++ application/models/Emp_clientsm.php
@@ -9,7 +9,17 @@ class Emp_clientsm extends CI_Model
 
     public function getClients($id = null)
     {
-        return $this->db->query('SELECT * FROM clients INNER JOIN clients_info ON clients.id_lead = clients_info.id_lead WHERE created_by=' . $id . ' AND (clients.id_lead IN (SELECT id FROM lead_payment WHERE `type` = 4))');
+        return $this->db->query(
+            'SELECT
+            clients.emp_id as client_emp_id, clients.real_id as client_real_id, clients.id_lead, client_first,
+            client_middle, client_last, client_email, created_time, created_by, phone, other_phone, birthdate,
+            address, school, student_id, intake_date, program, reservation_fee, tuition_fee_depo, contract_fee,
+            deadline, deferral_intake, college, high_school, graduate_school, emp_first, emp_middle, emp_last
+            FROM clients INNER JOIN
+            clients_info ON clients.id_lead = clients_info.id_lead
+            INNER JOIN employee ON clients.created_by = ' . $id .'
+            WHERE clients.id_lead IN (SELECT id FROM lead_payment WHERE `type` = 4)'
+        );
     }
 
     public function getClientsByStage($stage)
